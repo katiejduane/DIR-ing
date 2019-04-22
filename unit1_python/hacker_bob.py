d0e024dbca3b89313fd61ecaebe1d0c100e682d7
Katie Duane <katiejduane@gmail.com>
2019-04-22 11:35:14 -0400

    hacker bob functionality finished

--- unit1_python/hacker_bob.py
+++ unit1_python/hacker_bob.py
@@ -1,41 +1,41 @@
+# secret_message = "ubefr"
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+def de_cipher(coded_message):
+    i = 0
+    decoded_message = ""
+    rotate_by = 13  
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    while i < len(coded_message):
+        letter = coded_message[i]
+        i = i + 1
+        if letter in alphabet:
+            index_in_alphabet = alphabet.index(letter)
+            new_index = index_in_alphabet + rotate_by
+            if new_index >= len(alphabet):
+                new_index = new_index - len(alphabet)
+            decoded_message = decoded_message + alphabet[new_index]
+    return decoded_message
 
-rotate_by = 13  # We're doing ROT 13
-
-# Create a variable with an empty string that we'll add decoded letters to.
-uncoded_message = "horse"
-coded_message = ""
-i = 0
-while i < len(uncoded_message):
-    letter = uncoded_message[i]
-    i = i + 1
-    if letter in alphabet:
-        index_in_alphabet = alphabet.index(letter)
-        new_index = index_in_alphabet + rotate_by
-        if new_index >= len(alphabet):
-            new_index = new_index - len(alphabet)
-        coded_message = coded_message + alphabet[new_index]
-
-print(coded_message)
-
+# print(de_cipher(de_cipher(secret_message)))
 
 
-# thePassword = str(1234)
+thePassword = de_cipher('ubefr')
+
+def crack_password(password):
+    password_guess = input('guess my password: ')
+    i = 0
+    for char in password_guess:
+        coded = de_cipher(char)
+        if coded == password[i]:
+            print(f"you guessed {char} correctly!")
+            i += 1
+        else: 
+            print("try again!")
+            i += 1
 
 
-# def crack_password(password):
-#     passwd = list(thePassword)
-#     for i in range(0, 10):
-#         for j in range(0, 10):
-#             for k in range(0, 10):
-#                 for l in range(0, 10):
-#                     if str(i) == passwd[0] and str(j) == passwd[1] and str(k) == passwd[2] and str(l) == passwd[3]:
-#                         mylist = [str(i), str(j), str(k), str(l)]
-#                         code = "".join(mylist)
-#                         #print mylist
-#                         #print code
-#     print("Four Digit Password: {}".format(code))
+crack_password(thePassword)
 
-
-# crack_password(1234)
+# assignment: it takes a string (user input, letters only), we're going to have a hash
+# that reps the caesar cipher. for every char in the string, run a caeser cipher
+# function to see if that matches the hashed password
