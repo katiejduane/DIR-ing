0f93e2655ad5153060942bcceb6676d7ebd6cf3e
Katie Duane <katiejduane@gmail.com>
2019-04-24 15:38:29 -0400

    yikes

--- unit1_python/pygame-master/template.py
+++ unit1_python/pygame-master/template.py
@@ -1,18 +1,34 @@
 import pygame
 
+KEY_UP = 273
+KEY_DOWN = 274
+KEY_LEFT = 276
+KEY_RIGHT = 275
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self, image, pos):
+    def __init__(self, image, pos, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.center = pos
+        self.speed_x = 0
+        self.speed_y = 0
 
+
+# [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
 class Hero(Character):
-    pass
+    def __init__(self, image, pos, x, y):
+        super() .__init__(image, pos, x, y)
+        self.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
+        self.x = x
+        self.y = y
+        self.vx = 5
+        self.vy = 5
+
 
 class Monster(Character):
     pass
+
 
 def main():
     width = 512
@@ -29,8 +45,8 @@ def main():
     hero_image = pygame.image.load('images/hero.png').convert_alpha()
     monster_image = pygame.image.load('images/monster.png').convert_alpha()
 
-    player = Hero(hero_image, [250, 250])
-    monster = Monster(monster_image, [100, 100])
+    player = Hero(hero_image, [250, 250], 250, 250)
+    monster = Monster(monster_image, [100, 100], 100, 100)
 
     player_group = pygame.sprite.Group()
     player_group.add(player)
@@ -44,12 +60,33 @@ def main():
         for event in pygame.event.get():
 
             # Event handling
-
             if event.type == pygame.QUIT:
                 stop_game = True
 
-
         # Game logic
+        key = pygame.key.get_pressed()
+
+        # for i in range(2):
+            # if key[player.move[i]]:
+            #     player.rect.x += player.vx * [-1, 1][i]
+            #     print(player.rect.x, (player.vx * [-1, 1][i]))
+        if key[pygame.K_LEFT]:
+            player.rect.x += player.vx * -1
+            print(player.rect.x, (player.vx * -1))
+        if key[pygame.K_RIGHT]:
+            player.rect.x += player.vx * 1
+            print(player.rect.x, (player.vx * 1))
+        # for i in range(2):
+        #     if key[player.move[2:4][i]]:
+        #         player.rect.y += player.vy * [-1, 1][i]
+        #         print(player.rect.y, (player.vy * [-1, 1][i]))
+        if key[pygame.K_UP]:
+            player.rect.y += player.vy * -1
+            print(player.rect.y, (player.vy * -1))
+        if key[pygame.K_DOWN]:
+            player.rect.y += player.vy * 1
+            print(player.rect.y, (player.vy * 1))
+        
 
         # Draw background
         # screen.fill(blue_color)
@@ -60,7 +97,6 @@ def main():
 
 
         # Game display
-
         pygame.display.update()
         clock.tick(60)
 
