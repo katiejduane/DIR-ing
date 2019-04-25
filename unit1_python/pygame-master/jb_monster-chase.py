b2a51485fa7d400b8f3e199f450aa5d6c33831f0
jasonpbenson <jasonbenson3rd@gmail.com>
2019-04-25 11:33:09 -0400

    collisions = SAD

--- unit1_python/pygame-master/jb_monster-chase.py
+++ unit1_python/pygame-master/jb_monster-chase.py
@@ -1,12 +1,13 @@
 import pygame
+from pygame.locals import *
+from pygame.sprite import Sprite, Group, groupcollide
 import random
 from random import randint
-from pygame.sprite import Sprite, Group, groupcollide
 
-class Character(pygame.sprite.Sprite):
+class Character(Sprite):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.rect = pygame.Rect(0,0,32,32)
+        super(Character, self).__init__()
+        # self.rect = pygame.Rect(0,0,32,32)
         self.speed_x = 0
         self.speed_y = 0
 
@@ -15,15 +16,17 @@ class Hero(Character):
         super() .__init__()
         self.x = x
         self.y = y
+        self.image = pygame.image.load('images/hero.png')
+        self.rect = self.image.get_rect()
         self.rect.centerx = self.x
         self.rect.top = self.y 
-        self.speed = 8
+        self.speed = 4
         self.should_move_down = False
         self.should_move_up = False
         self.should_move_left = False
         self.should_move_right = False
 
-    def shouldMove(self, direction, start = True):
+    def should_move(self, direction, start = True):
         if direction == "right":
             self.should_move_right = start
         if direction == "left":
@@ -31,7 +34,7 @@ class Hero(Character):
         if direction == "up":
             self.should_move_up = start
         if direction == "down":
-            self.should_move_down = start      
+            self.should_move_down = start     
 
     def draw_me(self,w,h):
         if(self.should_move_right):
@@ -46,17 +49,24 @@ class Hero(Character):
         elif self.should_move_up:
             if(self.y >= 32):
                 self.y -= self.speed  
+        self.rect.x = self.x 
+        self.rect.y = self.y
+    
+    def catch_monster(self, enemy):
+        if self.x == enemy.x and self.y == enemy.y:
+            print("collision detected")
+            enemy.kill()
 
 class Monster(Character):
     def __init__(self, x, y):
         super() .__init__()
         self.x = x
         self.y = y
-        self.speed = 10
-        self.should_move_right = False
-        self.should_move_left = False
-        self.should_move_up = False
-        self.should_move_down = False
+        self.image = pygame.image.load('images/monster.png')
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.x
+        self.rect.top = self.y 
+        self.speed = 8
     
     def random_move(self,w,h):
         movements = ["right", "up right", "down right", "left", "up left", "down left", "up", "down"]
@@ -122,16 +132,21 @@ class Monster(Character):
             else:
                 self.y = h
                 self.y -= self.speed 
-        pygame.time.delay(50)
+        self.rect.x = self.x 
+        self.rect.y = self.y
+        # pygame.time.delay(50)
+    
+    def kill(self):
+        pygame.sprite.Sprite.kill(self)
 
 # instantiate Hero object & create sprite group
 the_hero = Hero(100, 100)
-heroes = Group()
+heroes = pygame.sprite.Group()
 heroes.add(the_hero)
 
 # instantiate Monster object & create sprite group
 the_monster = Monster(250, 250)
-monsters = Group()
+monsters = pygame.sprite.Group()
 monsters.add(the_monster)
 
 def main():
@@ -142,10 +157,7 @@ def main():
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Monster Chase')
 
-    background_image = pygame.image.load('images/background.png').convert_alpha()
-    hero_image = pygame.image.load('images/hero.png').convert_alpha()
-    monster_image = pygame.image.load('images/monster.png').convert_alpha()
-    
+    background_image = pygame.image.load('images/background.png')
 
     # Game initialization
     stop_game = False
@@ -157,38 +169,42 @@ def main():
             if event.type == pygame.QUIT:
                 stop_game = True
 
+            # event listener for keydown
             elif event.type == pygame.KEYDOWN:
                 if event.key == 275:
-                    the_hero.shouldMove("right")
+                    the_hero.should_move("right")
                 elif event.key == 276: 
-                    the_hero.shouldMove("left")
-                elif event.key == 273: #up arrow
-                    the_hero.shouldMove("up")
-                elif event.key == 274: #down arrow
-                    the_hero.shouldMove("down")
+                    the_hero.should_move("left")
+                elif event.key == 273: 
+                    the_hero.should_move("up")
+                elif event.key == 274: 
+                    the_hero.should_move("down")
+
             elif event.type == pygame.KEYUP:
                 if event.key == 275: 
-                    the_hero.shouldMove("right", False)
+                    the_hero.should_move("right", False)
                 elif event.key == 276: #left arrow
-                    the_hero.shouldMove("left", False)
+                    the_hero.should_move("left", False)
                 if event.key == 273: #up arrow
-                    the_hero.shouldMove("up", False)
+                    the_hero.should_move("up", False)
                 elif event.key == 274: #down arrow
-                    the_hero.shouldMove("down", False) 
+                    the_hero.should_move("down", False) 
 
-        # Game logic
+        # Draw game
+        if stop_game == False:
+            screen.blit(background_image, [0,0])
 
-        the_monster.random_move(512, 480)
-        the_hero.draw_me(512,480) 
+            for the_monster in monsters:
+                screen.blit(the_monster.image, [the_monster.x, the_monster.y])
+                the_monster.random_move(512, 480)
 
-        # Draw background
-        # screen.fill(blue_color)
-        screen.blit(background_image, [0,0])
-        screen.blit(hero_image, [the_hero.x, the_hero.y])
-        screen.blit(monster_image, [the_monster.x, the_monster.y])
+            for the_hero in heroes: 
+                the_hero.draw_me(512,480) 
+                the_hero.catch_monster(the_monster)
+                screen.blit(the_hero.image, [the_hero.x, the_hero.y])
 
         # Game display
-        pygame.display.update()
+        pygame.display.flip()
 
     pygame.quit()
 
