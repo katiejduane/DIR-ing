02cdc5225114bc4810dcc1bd98ff2a34850ea4b1
Katie Duane <katiejduane@gmail.com>
2019-05-31 11:14:59 -0400

    new shuffle, for evan

---
+++ fridayLessons/shuffleStudents.js
@@ -0,0 +1,19 @@
+let students = ["Liz", "Brodie", "Reagan", "Tom", "Jack F", "Ben", "Jack M", "Evan",
+    "Thomas", "Igor", "RJ", "Pierson", "JC", "Angel", "Austin", "KC", "Danny", "Eli"]
+
+function getRandom(arr){
+    return Math.floor(Math.random() * arr.length)
+}
+
+function shuffle(arr){
+    let shuffledStudents = []
+    while(arr.length !== 0){
+        let random = getRandom(arr)
+        let thisStudent = arr.splice(random, 1)
+        shuffledStudents.push(thisStudent[0])
+    }
+    return shuffledStudents
+}
+
+console.log(shuffle(students))
+
